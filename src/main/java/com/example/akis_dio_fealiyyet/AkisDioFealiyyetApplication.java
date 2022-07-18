d22a480b7908f210eb3c534698bdbde2dc98c6e2
ResadAsl <resadaslan007@gmail.com>
2022-07-18 17:05:42 +0400

    Initial commit

---
+++ src/main/java/com/example/akis_dio_fealiyyet/AkisDioFealiyyetApplication.java
@@ -0,0 +1,13 @@
+package com.example.akis_dio_fealiyyet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkisDioFealiyyetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkisDioFealiyyetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/akis_dio_fealiyyet/AkisDioFealiyyetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.akis_dio_fealiyyet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkisDioFealiyyetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
